40741000351ddce40974dbadc6d8b0e801843d4e
itsAnkitAkm <itsamkankit@gmail.com>
2026-01-23 02:26:03 +0530

    Add the socket connection

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -50,11 +50,13 @@ cache_element *head; // head of the cache lindeklist
 int cache_size;      // size of teh cache
 
 int main(int agrc, char* argv[]){
-    int client_scoketId, client_len;
+    int client_scoketId, client_len; // client_socketId == to store the client socket id
 
-    struct sockaddr server_addr, client_addr;
-    sem_inti(&semaphore, 0, MAX_CLIENTS);
+    struct sockaddr_in server_addr, client_addr;  //gerneric structure that store IP address+Port;
+    sem_inti(&semaphore, 0, MAX_CLIENTS); // maximum semaphores 
     pthread_mutex_init(&lock, NULL);
+
+    // check if we had the valid argument to start the program
     if(argv == 2){
         port_number = atoi(argv[1]);
     }else{
@@ -62,16 +64,58 @@ int main(int agrc, char* argv[]){
     }
 
     printf("Starting Proxy server at port: %d\n", port_number);
-    proxy_socketId = socket(AF_INET, SOCK_STREAM, 0);
+    proxy_socketId = socket(AF_INET, SOCK_STREAM, 0); // create the proxy soxket
     if(proxy_socketId<0){
         perror("Failed to create a socket\n");
         exit(1);
     }
     int reuse = 1;
+    //Allows reusing the same port immediately after restart.
     if(setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse))<0){
         perror("setSockOpt failed\n");
     }
 
-    //31:55
+    bzero((char *)&server_addr, sizeof(server_addr)); // just remove the garbage value
+
+    server_addr.sin_family = AF_INET; // tell the os that this is an IPv4 
+    server_addr.sin_port = htons(port_number); //Sets the port number your server listens on
+    server_addr.sin_addr.s_addr= INADDR_ANY; //accepts connections on ALL network interfaces
+
+   // Associate this socket (proxy_socketId) with this IP address + port (server_addr)
+    if(bind(proxy_socketId, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
+        perror("Port is not available\n");
+        exit(1);
+    }
+    printf("Binding on port %d\n", port_number);
+
+    int listen_status = listen(proxy_socketId, MAX_CLIENTS); // start listening on the socket with max queue size
+    if(listen_status<0){
+        perror("Error in listening\n");
+        exit(1);
+    }
+
+    int i=0;
+    int Connected_socketId[MAX_CLIENTS];
+
+    while(1){
+        bzero((char *)&client_addr, sizeof(client_addr));
+        client_len = sizeof(client_addr);
+        client_scoketId = accept(proxy_socketId, (struct sockaddr*)&client_addr, (socklen_t*)client_len);
+        if(client_scoketId<0){
+            printf("Not able to connect");
+            exit(1);
+        }else{
+            Connected_socketId[i]=client_scoketId;
+        }
+        struct sockaddr_in * client_pt =  (struct sockaddr_in *)& client_addr;
+        struct in_addr ip_addr = client_pt->sin_addr;
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &ip_addr, str, INET6_ADDRSTRLEN);
+        printf("Client is connected with port number %d and ip address is %s\n, n", ntohs(client_addr.sin_port), str);
+        pthread_create(&tid[i],NULL,thread_fn, (void*)&Connected_socketId[i]); // Creating a thread for each client accepted
+		i++; 
+    }
+    close(proxy_socketId);
     return 0;
+    
 }
